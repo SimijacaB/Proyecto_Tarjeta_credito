2c0210890c78366dcd2fce66d3a0796e49bb37e9
SimijacaB <yesikasimi@gmail.com>
2025-06-07 18:57:28 -0500

    first commit

---
+++ FrontedTarjetaCredito/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { TarjetaCredito } from './components/tarjeta-credito/tarjeta-credito';
+
+export const routes: Routes = [
+    {	
+        path: '',
+        component: TarjetaCredito
+    }
+];
---
+++ FrontedTarjetaCredito/src/app/components/tarjeta-credito/tarjeta-credito.ts
@@ -0,0 +1,13 @@
+import { Component, EventEmitter, Output, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tarjeta-credito',
+  imports: [],
+  templateUrl: './tarjeta-credito.html',
+  styleUrl: './tarjeta-credito.css'
+})
+export class TarjetaCredito {
+
+  
+
+}
